626cc137bc066daf2309d24e7a6729f8806b2983
hovhannesut <marmtest2@mail.ru>
2017-09-22 16:16:28 +0400

    init

--- RottenTomatoes/rt/rt/spiders/sitemap.py
+++ RottenTomatoes/rt/rt/spiders/sitemap.py
@@ -5,8 +5,12 @@ import scrapy
 class SitemapSpider(scrapy.Spider):
     name = 'sitemap'
     allowed_domains = ['rottentomatoes.com']
-    start_urls = ['http://rottentomatoes.com/sitemap.xml']
+    start_urls = ['http://www.rottentomatoes.com/sitemap.xml']
 
     def parse(self, response):
-        print("Hello")
-        pass
+        if (response.url == self.start_urls[0]):
+            urls = response.xpath("//*[name()='loc']/text()").extract()
+            for url in urls:
+                yield scrapy.Request(url)
+        else:
+            print("Hello")
